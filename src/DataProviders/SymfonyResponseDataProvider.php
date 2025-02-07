01ee7d0cfe9328b565d0cfda86c7fef24ab9350b
Bhushan <bhushangaykawad@gmail.com>
2025-02-07 12:21:05 +0530

    WIP

---
+++ src/DataProviders/SymfonyResponseDataProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Treblle\Symfony\DataProviders;
+
+use Treblle\Php\DataTransferObject\Request;
+use Treblle\Php\Contract\RequestDataProvider;
+
+final class SymfonyResponseDataProvider implements RequestDataProvider
+{
+    public function getRequest(): Request
+    {
+    }
+}
